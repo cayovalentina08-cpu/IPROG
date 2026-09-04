86135301dcf4fed9b6d4495a046098891b1e31ed
cayovalentina08-cpu <cayovalentina08@gmail.com>
2026-09-04 14:54:24 -0300

    Update sistema_pizzeria.py

--- sistema_pizzeria.py
+++ sistema_pizzeria.py
@@ -3,3 +3,13 @@ def calcular_descuento(subtotal):
         descuento = subtotal * 0.10
     else:
         descuento = 0
+        
+    return descuento
+
+
+# Pedir cantidad
+cantidad = int(input("Cantidad de pizzas: "))
+
+while cantidad <= 0:
+    print("La cantidad debe ser mayor que 0.")
+    cantidad = int(input("Cantidad de pizzas: "))
